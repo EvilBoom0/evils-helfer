552081e5f57813118b3bdc68d47d137a12fa22cf
EvilBoom0 <vonwieckikarsten@gmail.com>
2025-03-20 21:25:19 +0200

    Update user.js

--- models/user.js
+++ models/user.js
@@ -2,9 +2,9 @@ const mongoose = require("mongoose");
 
 const userSchema = new mongoose.Schema({
   userId: { type: String, required: true, unique: true },
-  credits: { type: Number, default: 1000 }, // Bargeld
-  bank: { type: Number, default: 0 }, // Bankguthaben
-  bankLimit: { type: Number, default: 1000 }, // Maximales Bankguthaben
+  credits: { type: Number, default: 1000 },
+  bank: { type: Number, default: 0 },
+  bankLimit: { type: Number, default: 1000 }
 });
 
 module.exports = mongoose.model("User", userSchema);
