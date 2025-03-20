022a85d1f62ce6aaefb9f4bcf430041bc6f32be5
EvilBoom0 <vonwieckikarsten@gmail.com>
2025-03-20 23:56:50 +0200

    Update xpSystem.js

--- utils/xpSystem.js
+++ utils/xpSystem.js
@@ -1,52 +1,74 @@
-const mongoose = require("mongoose");
-const User = require("../models/user");
-
-async function addXP(userId, xpGain) {
-  let user = await User.findOne({ userId });
-  if (!user) {
-    user = new User({
-      userId,
-      xp: 0,
-      level: 1,
-      credits: 1000,
-      bank: 0,
-      bankLimit: 1000
+'use strict';
+const mongoose = require('mongoose');
+const { Schema, model, connect } = mongoose;
+async function connectDB() {
+  try {
+    await connect("mongodb://localhost:27017/discord_casino", {
+      useNewUrlParser: true,
+      useUnifiedTopology: true
     });
+    console.log("XP System: Connected to MongoDB");
+  } catch (error) {
+    console.error("XP System: MongoDB connection error", error);
   }
-  user.xp += xpGain;
-  let levelsGained = 0;
-  while (user.xp >= user.level * 100) {
-    user.xp -= user.level * 100;
-    user.level += 1;
-    levelsGained++;
-  }
-  await user.save();
-  return { xp: user.xp, level: user.level, levelsGained };
 }
-
+connectDB();
+const xpSchema = new Schema({
+  userId: { type: String, required: true, unique: true },
+  xp: { type: Number, default: 0 },
+  level: { type: Number, default: 1 }
+}, { timestamps: true });
+const XPModel = model("XP", xpSchema);
 async function getXP(userId) {
-  let user = await User.findOne({ userId });
-  if (!user) {
-    user = new User({
-      userId,
-      xp: 0,
-      level: 1,
-      credits: 1000,
-      bank: 0,
-      bankLimit: 1000
-    });
-    await user.save();
+  try {
+    let userXP = await XPModel.findOne({ userId });
+    if (!userXP) {
+      userXP = new XPModel({ userId });
+      await userXP.save();
+    }
+    return { xp: userXP.xp, level: userXP.level };
+  } catch (error) {
+    console.error("XP System getXP Error:", error);
+    return { xp: 0, level: 1 };
   }
-  return { xp: user.xp, level: user.level };
 }
-
-async function setXP(userId, xp) {
-  let user = await User.findOneAndUpdate(
-    { userId },
-    { xp: xp },
-    { new: true, upsert: true }
-  );
-  return { xp: user.xp, level: user.level };
+async function addXP(userId, xpToAdd) {
+  try {
+    let userXP = await XPModel.findOneAndUpdate(
+      { userId },
+      { $inc: { xp: xpToAdd } },
+      { new: true, upsert: true }
+    );
+    return { xp: userXP.xp, level: userXP.level };
+  } catch (error) {
+    console.error("XP System addXP Error:", error);
+    return { xp: 0, level: 1 };
+  }
 }
-
-module.exports = { addXP, getXP, setXP };
+async function setXP(userId, xp, level) {
+  try {
+    let userXP = await XPModel.findOneAndUpdate(
+      { userId },
+      { xp: xp, level: level },
+      { new: true, upsert: true }
+    );
+    return { xp: userXP.xp, level: userXP.level };
+  } catch (error) {
+    console.error("XP System setXP Error:", error);
+    return { xp: 0, level: 1 };
+  }
+}
+async function resetXP(userId) {
+  try {
+    let userXP = await XPModel.findOneAndUpdate(
+      { userId },
+      { xp: 0, level: 1 },
+      { new: true, upsert: true }
+    );
+    return { xp: userXP.xp, level: userXP.level };
+  } catch (error) {
+    console.error("XP System resetXP Error:", error);
+    return { xp: 0, level: 1 };
+  }
+}
+module.exports = { getXP, addXP, setXP, resetXP };
