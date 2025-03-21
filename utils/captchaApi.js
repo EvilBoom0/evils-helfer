e3a8bca82089b468eadcd6ab57865bd7d6f5ef87
EvilBoom0 <vonwieckikarsten@gmail.com>
2025-03-21 20:15:08 +0200

    Update captchaApi.js

--- utils/captchaApi.js
+++ utils/captchaApi.js
@@ -1,11 +1,16 @@
 const axios = require("axios");
 
 async function generateCaptcha() {
-  const response = await axios.get("https://captcha-api.com/api/captcha");
-  return {
-    image: response.data.image, // base64 PNG
-    answer: response.data.answer
-  };
+  try {
+    const response = await axios.get("https://captcha-api.com/api/captcha");
+    return {
+      image: response.data.image,
+      answer: response.data.answer
+    };
+  } catch (error) {
+    console.error("Captcha API Fehler:", error);
+    throw new Error("Captcha konnte nicht geladen werden.");
+  }
 }
 
 module.exports = { generateCaptcha };
